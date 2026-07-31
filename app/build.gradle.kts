acab7935d21be17950bbc8cf1459ae0b3f2ed265
Matteo Steri <matteo.steri@gmail.com>
2026-07-31 20:08:30 +0200

    v1.4.3 Pubblicazione aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.matteosteri.tripchecklist"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
+        versionCode = 15
         versionName = "1.4.3"
 
         testInstrumentationRunner= "androidx.test.runner.AndroidJUnitRunner"
